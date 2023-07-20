cd208b312cf749e117284ae963e531d5ef8f0f97
LuisMariaLucero <luismarialucero@gmail.com>
2023-07-20 10:27:59 -0300

    mi primer commit

---
+++ funcionpar.js
@@ -0,0 +1,9 @@
+const funcionDeNumeroPar = function esPar(number) {
+    if (number % 2 === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  module.exports = funcionDeNumeroPar;
