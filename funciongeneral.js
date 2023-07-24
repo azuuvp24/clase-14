de2955f6b739d1c067ad8e49368ca9620613c60e
LuisMariaLucero <luismarialucero@gmail.com>
2023-07-24 09:43:43 -0300

    agrega la funcion general

---
+++ funciongeneral.js
@@ -0,0 +1,34 @@
+const funcionDeNumeroPar = require("./funcionpar.js");
+const multiplicacion = require("./multiplicacion.js");
+
+// const funcionDeAzul = function(number){
+//   return number * 2; 
+// } // borrar esto dsp
+
+// function parDuplicado(array) {
+//     let arrayNuevo = [];
+//     for (let i = 0; i < array.length; i++) {
+//         if (funcionDeNumeroPar([i])) {
+//             arrayNuevo.push(funcionDeAzul([i]))
+//         }
+//     }
+//     return arrayNuevo;    
+    
+// } 
+
+
+function parDuplicado(array) {
+    let arrayNuevo = [];
+    for (let i = 0; i < array.length; i++) {
+      if (funcionDeNumeroPar(array[i])) {
+        arrayNuevo.push(multiplicacion(array[i]));
+      }
+    }
+    return arrayNuevo;
+  };
+
+// console.log(funcionDeAzul(2));
+
+// console.log(parDuplicado([1, 2, 4, 5, 6, 8])); 
+
+console.log(multiplicacion(2));
